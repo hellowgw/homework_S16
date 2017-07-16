fc13e5758bbdad483df000f0170a3fa6979a200a
hellowgw <hellowgw@gmail.com>
2017-07-17 01:07:10 +0800

    完成修改余额功能

--- day4/atm/api/create_user.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# coding:utf-8
-
-
-def adduser(username):
-    print('add user %s' % username)
--- day4/third.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# coding:utf-8
-
-import atm
-atm.api.create_user.adduser('wgw')
